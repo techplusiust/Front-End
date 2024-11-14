84cab1be48d6b96b91bf59a02c6b52b373b7b5c9
farg0l <fargolnasiri@gmail.com>
2024-11-14 19:16:45 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { UserContextProvider } from "./contexts/UserContext";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import ProfilePage from "./pages/ProfilePage";
+import Schedule from "./pages/schedule";
 
 function App() {
   return (
     <BrowserRouter>
-      <UserContextProvider>
-        <ToastContainer />
-        <Routes>
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </UserContextProvider>
+      <ToastContainer />
+      <Routes>
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />{" "}
+        <Route path="/schedule" element={<Schedule />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+declare module "react-week-calendar";
+declare module "moment/locale/fa";
+declare module "moment-jalaali";
