479ac98c7b92d2b9ef0c614714ef784ad5ad2173
farg0l <fargolnasiri@gmail.com>
2025-01-03 19:54:38 +0330

    api scheduling

--- src/components/schedule/utils/calculateRowSpan.tsx
+++ src/components/schedule/utils/calculateRowSpan.tsx
@@ -1,16 +1,28 @@
 import DateObject from "react-date-object";
+import gregorian from "react-date-object/calendars/gregorian";
+import gregorian_en from "react-date-object/locales/gregorian_en";
 
-export const calculateRowSpan = (start: string, end: string): number => {
+export const calculateRowSpan = (start: string, duration: number): number => {
+  console.log("duration: ", duration);
+
   const startTime = new DateObject({
-    date: `1403/01/01 ${start}`,
+    date: `2025/01/01 ${start}`,
     format: "YYYY/MM/DD HH:mm",
-  });
-  const endTime = new DateObject({
-    date: `1403/01/01 ${end}`,
-    format: "YYYY/MM/DD HH:mm",
+    calendar: gregorian, // اضافه کنید
+    locale: gregorian_en, // یا هر locale معتبر دیگه
   });
 
-  const diffInMinutes =
-    (endTime.toDate().getTime() - startTime.toDate().getTime()) / (1000 * 60);
+  const startUnix = startTime.unix;
+  console.log("startUnix: ", startUnix);
+  const endUnix = startTime.add(duration, "hours").unix;
+  console.log("endUnix: ", endUnix);
+  const diff = endUnix - startUnix;
+  console.log("diff: ", diff);
+
+  const diffInMinutes = diff / 60;
+  console.log("diffInMinutes: ", diffInMinutes);
+
+  // return diffInMinutes;
+
   return Math.ceil(diffInMinutes / 30); // Each slot is 30 minutes
 };
